c8448bc16358435910ddee43d611e64714741ab4
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-07-11 23:02:57 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,16 @@ app.get('/login', (req, res) => {
   res.render('login.ejs')
 })
 
+app.post('/login', (req, res) => {
+  
+})
+
 app.get('/register', (req, res) => {
   res.render('register.ejs')
 })
 
 app.post('/register', (req, res) => {
-
+  req.body.email
 })
 
 app.listen(3000)
