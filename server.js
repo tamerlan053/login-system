64da9f8e5e990624422deddfe51c410241cad4da
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-07-13 00:09:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
   try {
-    
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+    })
   } catch {
     
   }
