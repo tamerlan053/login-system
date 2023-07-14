caa6e02009953cc9a407513ceece85b4e46d7d4b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-07-14 23:28:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
 
-const users = []
+
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
