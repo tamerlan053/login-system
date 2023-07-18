00f67f3a4627eb99a8ba2a59d42945139ac481cb
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-07-19 00:08:05 +0200

    Update passport-config.js

--- passport-config.js
+++ passport-config.js
@@ -1,14 +1,20 @@
 const LocalStrategy = require('passport-local').Strategy
 
-function initialize(passport) {
-  const authenticateUser = (email, password, done) => {
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
     const user = getUserByEmail(email)
     if (user == null) {
       return done(null, false, { message: 'No user with that email' })
     }
 
     try {
-      
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user)
+      } else {
+        return done(null, false, { message: 'Password incorrect' })
+      }
+    } catch (e) {
+      return done(e)
     }
   }
 
