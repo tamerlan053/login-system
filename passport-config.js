6ed002d93e9a0c99edf77565d37941fae437a13f
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-07-19 00:05:52 +0200

    Update passport-config.js

--- passport-config.js
+++ passport-config.js
@@ -3,6 +3,9 @@ const LocalStrategy = require('passport-local').Strategy
 function initialize(passport) {
   const authenticateUser = (email, password, done) => {
     const user = getUserByEmail(email)
+    if (user == null) {
+      return done(null, false, { message: 'No user with that email' })
+    }
   }
 
   passport.use(new LocalStrategy({ usernameField: 'email' }), authenticateUser)
