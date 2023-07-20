963764d1d4343d9b24c085018f4f94cfd37c73de
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-07-21 00:14:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
 const passport = require('passport')
+const flash = require('express-flash')
 
 const initializePassport = require('./passport-config')
 initializePassport(passport)
